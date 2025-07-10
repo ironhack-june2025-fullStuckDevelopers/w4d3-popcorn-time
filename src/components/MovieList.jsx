19a91bb924590cc8c543b218ac8c2abe3c2032c2
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-10 11:46:55 +0200

    refactor: display number of movies in Header (lift state up)

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,36 +1,15 @@
-
-import { useState } from "react";
-
-import movies from "../data/movies.json"
 import MovieCard from "./MovieCard";
 
 
-function MovieList() {
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
-
-    
-    // deleteMovie: will receive the id of a movie to delete & update state with the new list of movies
-    const deleteMovie = (movieId) => {
-        const newList = moviesToDisplay.filter((movie) => {
-            return movie.id !== movieId;
-        })
-
-        // moviesToDisplay = newList; // NEVER, NEVER, MODIFY STATE DIRECTLY
-        setMoviesToDisplay(newList)
-    }
-
-
+function MovieList(props) {
     return (
         <>
-            <h1>Number of movies: {moviesToDisplay.length}</h1>
-
-            {moviesToDisplay.map((movieObj) => {
+            {props.moviesArr.map((movieObj) => {
                 return (
                     <MovieCard 
                         key={movieObj.id} 
                         movieDetails={movieObj}
-                        onDelete={deleteMovie}
+                        onDelete={props.onDelete}
                     />
                 )
             })}
